d87948c2cff9d60ab8085a602fef4b82f715b6eb
PalmaPedro <pmcpalma@gmail.com>
2022-07-04 16:40:03 +0200

    discovery service

--- customer/src/main/java/com/pedropalma/customer/CustomerService.java
+++ customer/src/main/java/com/pedropalma/customer/CustomerService.java
@@ -21,7 +21,7 @@ public class CustomerService {
 		// todo: check if fraudster
 		customerRepository.saveAndFlush(customer);
 		FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-						"http://localhost:8081/api/v1/fraud-check/{customerId}",
+						"http://FRAUD/api/v1/fraud-check/{customerId}",
 						FraudCheckResponse.class,
 						customer.getId()
 		);
