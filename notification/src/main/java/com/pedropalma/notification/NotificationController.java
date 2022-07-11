bb98d5a602e8ba017de9e48f31cf71361bb9b9da
PalmaPedro <pmcpalma@gmail.com>
2022-07-11 17:03:17 +0100

    added sleuth for distributed tracing

--- notification/src/main/java/com/pedropalma/notification/NotificationController.java
+++ notification/src/main/java/com/pedropalma/notification/NotificationController.java
@@ -3,6 +3,7 @@ package com.pedropalma.notification;
 import com.pedropalma.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,8 @@ public class NotificationController {
 
 	private final NotificationService notificationService;
 
-	public void setNotification(@RequestBody NotificationRequest notificationRequest){
+	@PostMapping
+	public void sendtNotification(@RequestBody NotificationRequest notificationRequest){
 		log.info("New notification... {}", notificationRequest);
 		notificationService.send(notificationRequest);
 	}
