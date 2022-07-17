f945ec1fc9a8519b68f71ce4d29a83b8cd51c64a
PalmaPedro <pmcpalma@gmail.com>
2022-07-17 17:11:44 +0100

    update NotificationApplication

--- notification/src/main/java/com/pedropalma/notification/NotificationApplication.java
+++ notification/src/main/java/com/pedropalma/notification/NotificationApplication.java
@@ -1,13 +1,37 @@
 package com.pedropalma.notification;
 
+import com.pedropalma.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+				scanBasePackages = {
+								"com.pedropalma.notification",
+								"com.pedropalma.amqp",
+				}
+)
 @EnableEurekaClient
 public class NotificationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(
+					RabbitMQMessageProducer producer,
+					NotificationConfig notificationConfig
+	) {
+		return args -> {
+			producer.publish(
+							new Person("Pedro", 40),
+							notificationConfig.getInternalExchange(),
+							notificationConfig.getInternalNotificationRoutingKey());
+		};
+	}
+
+	record Person (String name, int age){}
 }
+
